18a59616b003c28b5228029347ecf2110250e8f9
sofiaadam <sofiaadamkho@gmail.com>
2017-04-27 09:11:31 +0800

    alert for success

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,11 @@ class SessionsController < Clearance::BaseController
 
     def create
     @user = authenticate(params)
+    
 
     sign_in(@user) do |status|
       if status.success?
+        flash[:success] = "Succesfully logged in"
         redirect_back_or url_after_create
       else
         flash[:danger]= status.failure_message
@@ -13,5 +15,28 @@ class SessionsController < Clearance::BaseController
     end
   end
 
+    
+
+      
+
+    private
+
+    def redirect_signed_in_users
+      if signed_in?
+        redirect_to url_for_signed_in_users
+      end
+    end
+
+    def url_after_create
+      Clearance.configuration.redirect_url
+    end
+
+    def url_after_destroy
+      sign_in_url
+    end
+
+    def url_for_signed_in_users
+      url_after_create
+    end
 
 end
