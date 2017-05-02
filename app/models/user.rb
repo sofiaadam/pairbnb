936322c365338f65a9028d715b121ff9b5f02164
sofiaadam <sofiaadamkho@gmail.com>
2017-05-02 11:27:29 +0800

    authorization user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   include Clearance::User
+  require 'carrierwave/orm/activerecord'
   has_many :authentications, :dependent => :destroy
   has_many :listings
-  
+  mount_uploader :avatar, AvatarUploader
   enum role: [:customer, :moderator, :superadmin]
   enum gender: [:undefined, :male, :female]
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170429092841) do
+ActiveRecord::Schema.define(version: 20170502031313) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,8 @@ ActiveRecord::Schema.define(version: 20170429092841) do
     t.integer  "age"
     t.integer  "gender",                         default: 0
     t.integer  "role"
+    t.string   "image"
+    t.string   "avatar"
     t.index ["email"], name: "index_users_on_email", using: :btree
     t.index ["remember_token"], name: "index_users_on_remember_token", using: :btree
   end
