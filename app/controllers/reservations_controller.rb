be0cbbc08073929016b4fc2a41affdc7b7c4c769
sofiaadam <sofiaadamkho@gmail.com>
2017-05-04 11:40:02 +0800

    able to make reservations

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,28 +1,41 @@
 class ReservationsController < ApplicationController
- def create
+
+
+    def index
+        @reservation = Reservation.all
+    end
+    
+    def create
     #Finds all the information of the listing for this booking
-    @listing = Listing.find(params[:listing_id])
+        @listing = Listing.find(params[:listing_id])
 
     #creates a booking object with the current_user id, together with the params from the form
-    @reservation = current_user.reservations.new(reservation_params)
+        @reservation = current_user.reservations.new(reservation_params)
     
     #all the listing information is stored with the booking, which in this case is the listing id in booking
-    @reservation.listing = @listing
-    if @reservation.save
-      redirect_to current_user
-    else
-      @errors = reservation.errors.full_messages
-      render "listings/show"
+        @reservation.listing = @listing
+        if @reservation.save
+            redirect_to current_user
+            else
+            @errors = reservation.errors.full_messages
+            render "listings/show"
+        end
     end
-  end
 
-  def destroy
-    @reservation = Reservation.find(params[:id])
-    @reservation.destroy
-    redirect_to @reservation.user
-  end
+    def show
+        @reservation = Reservation.find(params[:id])
+    end
+    
 
-  private
+    
+    def destroy
+        @reservation = Reservation.find(params[:id])
+        @reservation.destroy
+        redirect_to @reservation.user
+    end
+
+    private
+    
     def reservation_params
         #what this does is to say that the listing object can only pass in these following params
         params.require(:reservation).permit(:start_date, :end_date)
