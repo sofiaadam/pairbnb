00e5694ceb18abd9a28c9ee333c8b02d3a1269b1
sofiaadam <sofiaadamkho@gmail.com>
2017-05-09 12:19:19 +0800

    pg_search migrated

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -13,8 +13,9 @@ class Listing < ApplicationRecord
    enum property_type: [:house, :apartment]
    enum room_type: [:shared_room, :private_room, :entire_room]
     
-   scope :bathroom, -> num {(where(bathroom:num))}
-   scope :min_max_price, -> (min,max) { where('price > ? AND price < ?', min, max) }
-
+#    scope :bathroom, -> num {(where(bathroom:num))}
+#    scope :min_max_price, -> (min,max) { where('price > ? AND price < ?', min, max) }
+multisearchable :against => [:address, :bathroom, :price]
+#YOU STOP HERE - before moving on to rspec
    
 end
